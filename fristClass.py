a3b3876ea02d347e0fad7efd5b1e91313b1e09c6
Rihanul Islam <rihanthedev@gmail.com>
2022-11-13 23:08:12 +0600

    Created using Colaboratory

---
+++ fristClass.py
@@ -0,0 +1,53 @@
+{
+  "nbformat": 4,
+  "nbformat_minor": 0,
+  "metadata": {
+    "colab": {
+      "provenance": [],
+      "authorship_tag": "ABX9TyPy8fSe4OrMinxURRDUR1tr",
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "name": "python3",
+      "display_name": "Python 3"
+    },
+    "language_info": {
+      "name": "python"
+    }
+  },
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Rihanulislam/Python-Basic-To-Advance/blob/main/fristClass.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "execution_count": 4,
+      "metadata": {
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        },
+        "id": "lOsX2e7w7vgK",
+        "outputId": "aa1eeb43-9e8a-40f4-ee5d-860db38cfbf5"
+      },
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "hello Raihan Sharif ! Welcome \n"
+          ]
+        }
+      ],
+      "source": [
+        "print('hello Raihan Sharif ! Welcome ')"
+      ]
+    }
+  ]
+}
