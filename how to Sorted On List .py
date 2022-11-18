819afb8b90658d27c676a59bec977ab804cbc94e
Rihanul Islam <rihanthedev@gmail.com>
2022-11-18 16:51:28 +0600

    sorted

---
+++ how to Sorted On List .py
@@ -0,0 +1,54 @@
+{
+  "cells": [
+    {
+      "cell_type": "markdown",
+      "metadata": {
+        "id": "view-in-github",
+        "colab_type": "text"
+      },
+      "source": [
+        "<a href=\"https://colab.research.google.com/github/Rihanulislam/Python-Basic-To-Advance/blob/main/how%20to%20Sorted%20On%20List%20.py\" target=\"_parent\"><img src=\"https://colab.research.google.com/assets/colab-badge.svg\" alt=\"Open In Colab\"/></a>"
+      ]
+    },
+    {
+      "cell_type": "code",
+      "source": [
+        "#python List Method.\n",
+        "#sorted\n",
+        "friend_list = [4, 3, 2, 1]\n",
+        "print(sorted(friend_list))"
+      ],
+      "metadata": {
+        "id": "wWO4dEb-WZxe",
+        "outputId": "93e5c248-1656-44db-8b7c-9a8414cfc9db",
+        "colab": {
+          "base_uri": "https://localhost:8080/"
+        }
+      },
+      "execution_count": 1,
+      "outputs": [
+        {
+          "output_type": "stream",
+          "name": "stdout",
+          "text": [
+            "[1, 2, 3, 4]\n"
+          ]
+        }
+      ]
+    }
+  ],
+  "metadata": {
+    "colab": {
+      "name": "Welcome To Colaboratory",
+      "toc_visible": true,
+      "provenance": [],
+      "include_colab_link": true
+    },
+    "kernelspec": {
+      "display_name": "Python 3",
+      "name": "python3"
+    }
+  },
+  "nbformat": 4,
+  "nbformat_minor": 0
+}
